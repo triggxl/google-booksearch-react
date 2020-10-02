7712c1a977a6496659a9585274cef82afee1a755
triggxl <triggxl@hotmail.com>
2020-10-02 14:05:35 -0400

    added objectives in comments

--- src/App.js
+++ src/App.js
@@ -100,6 +100,12 @@ class App extends React.Component {
 }
 
 /*
+Objective:
+-Provide a search form to allow users to search for books by some search term that they enter.
+-Allow filters such as type of book or whether the book is a free ebook or not.
+-Display the search result in list and optionally allow the user to click a result to see further details of the book.
+-Use the Google Books API to perform the search
+
 Template for methods on lines 41 & 46
 
 import React from 'react';
